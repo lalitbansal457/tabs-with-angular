4ebfefa6ce9531c6c599ab6ad5777243d050a98a
lalit bansal <lalit.bansal457@gmail.com>
2018-05-25 15:27:52 +0530

    Adding comments

--- controller.js
+++ controller.js
@@ -3,10 +3,13 @@ var app = angular.module('myApp', []);
 app.controller("tabController", function($scope){
 	$scope.numList = ["One", "Two", "Three", "Four"];
 	$scope.tab = 0;
+	
+	// set current active tab index
 	$scope.clickMe = function(index){
 		$scope.tab = index;
 	};
 
+	// check if tab index 
 	$scope.isSet = function(tabId){
 		return $scope.tab ===  tabId;
 	};
